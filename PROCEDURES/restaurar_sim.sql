0f5cdef10f2a20f4c2d1f6ce738cf7e07752b881
Jorge Cróquer <jacroquer.18@est.ucab.edu.ve>
2021-06-30 13:56:49 -0400

    Listos los parametros iniciales

--- PROCEDURES/restaurar_sim.sql
+++ PROCEDURES/restaurar_sim.sql
@@ -1,3 +1,5 @@
+--Este procedure sirve para retornar la simulacion al punto inicial
+
 CREATE OR REPLACE PROCEDURE RESTAURAR_SIM IS
     tabla VARCHAR2(12) := 'H_HABITANTES';
 
@@ -16,7 +18,13 @@ BEGIN
     UPDATE PAIS_GE pg SET pg.cant_hab_pge.cant_infectados = 70, pg.cant_hab_pge.cant_recuperados = 0, pg.cant_hab_pge.cant_fallecidos = 0 WHERE pg.pais_pge = 11;
     UPDATE PAIS_GE pg SET pg.cant_hab_pge.cant_infectados = 5, pg.cant_hab_pge.cant_recuperados = 0, pg.cant_hab_pge.cant_fallecidos = 0 WHERE pg.pais_pge = 12;
     UPDATE PAIS_GE pg SET pg.cant_hab_pge.cant_infectados = 450, pg.cant_hab_pge.cant_recuperados = 0, pg.cant_hab_pge.cant_fallecidos = 0 WHERE pg.pais_pge = 13;
+    UPDATE PAIS_GE pg SET pg.cant_hab_pge.cant_infectados = 575, pg.cant_hab_pge.cant_recuperados = 0, pg.cant_hab_pge.cant_fallecidos = 0 WHERE pg.pais_pge = 14;
+    UPDATE PAIS_GE pg SET pg.cant_hab_pge.cant_infectados = 700, pg.cant_hab_pge.cant_recuperados = 0, pg.cant_hab_pge.cant_fallecidos = 0 WHERE pg.pais_pge = 15;
+    UPDATE PAIS_GE pg SET pg.cant_hab_pge.cant_infectados = 3, pg.cant_hab_pge.cant_recuperados = 0, pg.cant_hab_pge.cant_fallecidos = 0 WHERE pg.pais_pge = 16;
+    UPDATE PAIS_GE pg SET pg.cant_hab_pge.cant_infectados = 3000, pg.cant_hab_pge.cant_recuperados = 0, pg.cant_hab_pge.cant_fallecidos = 0 WHERE pg.pais_pge = 17;
     UPDATE PAIS_GE pg SET pg.cant_hab_pge.cant_infectados = 1, pg.cant_hab_pge.cant_recuperados = 0, pg.cant_hab_pge.cant_fallecidos = 0 WHERE pg.pais_pge = 18; 
+    UPDATE PAIS_GE pg SET pg.cant_hab_pge.cant_infectados = 1150, pg.cant_hab_pge.cant_recuperados = 0, pg.cant_hab_pge.cant_fallecidos = 0 WHERE pg.pais_pge = 19; 
+    UPDATE PAIS_GE pg SET pg.cant_hab_pge.cant_infectados = 35, pg.cant_hab_pge.cant_recuperados = 0, pg.cant_hab_pge.cant_fallecidos = 0 WHERE pg.pais_pge = 20;
     UPDATE EVENTOS_ALEATORIOS SET habilitado_eve = 'Y' WHERE tipo_eve = 'CUARENTENA';
     UPDATE PAIS SET tasa_repro_pai = 3 WHERE id_pai = 1;
     UPDATE PAIS SET tasa_repro_pai = 1.5 WHERE id_pai = 2;
@@ -31,5 +39,11 @@ BEGIN
     UPDATE PAIS SET tasa_repro_pai = 1.1 WHERE id_pai = 11;
     UPDATE PAIS SET tasa_repro_pai = 1.85 WHERE id_pai = 12;
     UPDATE PAIS SET tasa_repro_pai = 1.45 WHERE id_pai = 13;
+    UPDATE PAIS SET tasa_repro_pai = 3.2 WHERE id_pai = 14;
+    UPDATE PAIS SET tasa_repro_pai = 2.5 WHERE id_pai = 15;
+    UPDATE PAIS SET tasa_repro_pai = 1.5 WHERE id_pai = 16;
+    UPDATE PAIS SET tasa_repro_pai = 2.4 WHERE id_pai = 17;
     UPDATE PAIS SET tasa_repro_pai = 1.55 WHERE id_pai = 18;
+    UPDATE PAIS SET tasa_repro_pai = 1.15 WHERE id_pai = 19;
+    UPDATE PAIS SET tasa_repro_pai = 1.5 WHERE id_pai = 20;
 END;
