b6d5f686e307397d3fed56ba379191396465a341
LeoPro420 <jlcontreras.18@est.ucab.edu.ve>
2021-07-03 18:31:28 -0400

    Leo:Refinado el modulo de economía en base a pruebas, lo que está funciona bien, falta el flujo donde los paises piden a covax y la funcion  solicitud a covax esta casi lista

---
+++ Modulos/solicitud_a_covax.sql
@@ -0,0 +1,77 @@
+CREATE OR REPLACE FUNCTION todos_20 RETURN BOOLEAN AS
+porcentaje_equivalente NUMBER;
+BEGIN
+    FOR p IN(SELECT id_pai, SUM(cantidad_dis) as cantidad_vac
+            FROM PAIS
+            JOIN ORDEN ON pais_ord = id_pai
+            JOIN DISTRIBUCION ON n_orden_dis = id_ord
+            GROUP BY id_pai)
+    LOOP
+        porcentaje_equivalente := TRUNC(p.cantidad_vac/get_poblacion(p.id_pai,'TOTAL')*100,2)
+        IF(porcentaje_equivalente < 20)THEN
+            RETURN (FALSE);
+        END IF;
+    END LOOP;
+    RETURN (TRUE)
+END;
+
+CREATE OR REPLACE FUNCTION porcentaje_restante(pais_p NUMBER) RETURN NUMBER AS --Podría sustituir a "meta superada" en el moódulo de economía
+porcentaje_equivalente NUMBER;
+cantidad_vac NUMBER;
+BEGIN
+    SELECT SUM(cantidad_dis)
+    INTO cantidad_vac
+    FROM PAIS
+    JOIN ORDEN ON pais_ord = id_pai
+    JOIN DISTRIBUCION ON n_orden_dis = id_ord
+    WHERE id_pai = pais_p;
+    porcentaje_equivalente := TRUNC(cantidad_vac/get_poblacion(pais_p,'TOTAL')*100,2)
+    RETURN porcentaje_restante;
+END;
+
+CREATE OR REPLACE FUNCTION solicitar_orden_covax(pais_p NUMBER) RETURN BOOLEAN AS
+orden_a_aprobar ORDEN%rowtype;
+pago_restante NUMBER;
+porcentaje_restante;
+BEGIN
+    IF(todos_20() = TRUE)THEN
+        --CUANTO LE FALTA PARA LLEGAR A LA META, ¿A LA META O DE SU POBLACIÓN?
+        --VACUNAS QUE ACEPTA
+        IF(porcentaje_restante(pais_p) < 30) THEN
+            --Envía con lo que falta
+            RETURN(TRUE);
+        ELSE
+            --Envia 30
+            RETURN(TRUE);
+        END IF;
+    ELSE
+        IF(porcentaje_restante(pais_p) < 20) THEN
+            SELECT *
+            INTO orden_a_aprobar
+            FROM orden
+            WHERE pais_ord = pais_p 
+            AND distribuidora_ord = 10 
+            AND estatus_ord = 'EN ESPERA'; --Obtenemos la orden en espera, solo debería haber 1
+            
+            UPDATE ORDEN SET estatus_ord = 'EN TRANSITO' WHERE id_ord = orden_a_aprobar.estatus_ord ; --Ponemos en transito la orden de ese pais a covax (10)
+            
+            SELECT orden_a_aprobar.monto_a_pagar - SUM(monto_pag) 
+            INTO pago_restante
+            FROM PAGO
+            WHERE n_orden_pag = orden_a_aprobar.id_ord;
+            IF(pago_restante> 0) THEN --Realizamos el pago pendiente de ser necesario 
+                INSERT INTO PAGO VALUES(DEFAULT,orden_a_aprobar.f_entrega_ord,pago_restante,orden_a_aprobar.id_ord);
+            END IF;
+            DBMS_OUTPUT.PUT_LINE('Pago completado');
+        ELSE
+            RETURN (FALSE);
+        END IF;
+    END IF;
+END;
+
+
+--Entonces lo que jesus hizo es que cuando registra las primeras ordenes las pone con el estatus "en espera"
+-- lo que yo tengo que hacer es que cuando recibo un pais que le falta su 20% y tiene una orden en espera
+--es cambiarle el estatus en entransito para que luego el la espere como orden pendiente y registrar el pago
+
+--Tengo que arreglar el tema de las vacunas restringidas y agregarlo a mi codigo
