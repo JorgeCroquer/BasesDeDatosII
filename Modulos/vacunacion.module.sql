aa1e9e7f17d0652b6b90c5ff6ae16c1180aa86e5
Jorge Cróquer <jacroquer.18@est.ucab.edu.ve>
2021-07-02 22:57:29 -0400

    Modulo de vacunacion listo Probado con pocos registros Faltan pruebas de integracion

--- Modulos/vacunacion.module.sql
+++ Modulos/vacunacion.module.sql
@@ -19,8 +19,25 @@ create or replace NONEDITIONABLE PROCEDURE vacunacion(fecha_actual DATE) IS
         FROM INVENTARIO_VAC
         WHERE centro_vac_inv = centro AND cantidad_pri_inv > 0 AND cantidad_seg_inv > 0;
 
+    CURSOR porcentajes_vacuna(centro NUMBER) IS
+        SELECT vacuna_inv, TRUNC(cantidad_pri_inv/(SELECT SUM(cantidad_pri_inv)
+                                                    FROM INVENTARIO_VAC
+                                                    WHERE centro_vac_inv = centro),3) AS porcentaje
+        FROM INVENTARIO_VAC
+        WHERE centro_vac_inv = centro;
+
+
     pendientes JORNADA_VAC%ROWTYPE; --Guarda el registro de hace 21 dias (3 semanas)
+
+
+    porcentaje_vacunados NUMBER; --Guarda el procentaje de vacunados con 1era dosis de un grupo etario
+                                 --con respecto a un pais
+
+    pri_dosis_disponibles NUMBER; --Guarda las primeras dosis disponibles de una vacuna en un centro
     
+    existencia NUMBER;
+    
+    segundas_dosis NUMBER;
 
 BEGIN
 
@@ -35,6 +52,17 @@ BEGIN
                                        centro.cant_alcohol_sum,
                                        centro.cant_algodon_sum,
                                        centro.cant_par_guantes_sum);
+                    
+        --Primero hay que crear todas las jornadas
+        FOR vac IN 1..8 
+        LOOP
+            FOR grupo IN 1..4
+            LOOP
+                --Inserto una jornada por cada centro, cada vacuna y cada grupo etario
+                INSERT INTO JORNADA_VAC VALUES (fecha_actual,0,0,centro.id_cen,vac,centro.pais_cv,grupo);
+            END LOOP;
+        END LOOP;
+
         --Primero hay que ver si hay dosis pendientes en cada jornada de cada vacuna y grupo
         FOR inventario IN inventarios(centro.id_cen)
         LOOP
@@ -43,7 +71,7 @@ BEGIN
             LOOP
                 DBMS_OUTPUT.PUT_LINE('grupo -> ' || grupo);
                 --Primero abre la jornada 
-                INSERT INTO JORNADA_VAC VALUES (fecha_actual,0,0,centro.id_cen,inventario.vacuna_inv,centro.pais_cv,grupo);
+                
 
 
                 BEGIN
@@ -69,10 +97,7 @@ BEGIN
                     THEN
                         DBMS_OUTPUT.PUT_LINE('Si hay suministros' );
 
-                        --Guarda el inventario del centro con la vacuna especifica
-                        --SELECT * INTO inventario
-                        --FROM INVENTARIO_VAC inv
-                        --WHERE inv.centro_vac_inv = centro.id_cen AND inv.vacuna_inv = vac.vacuna_inv;
+                        
 
                         --Chequea la existencia de segundas dosis suficientes en el inventario
                         IF (inventario.cantidad_seg_inv > pendientes.cantidad_pri_jv) THEN 
@@ -110,9 +135,190 @@ BEGIN
 
                 END IF;
             END LOOP;
+
+            --guarda la cantidad de primera dosis que tiene el centro
+            
         END LOOP;
 
+        --Sigue con las primeras dosis
+
+
+        --Esta query devuelve la cantidad de nuevas primeras dosis a poner
+        --dependiendo de las segundas dosis que ya puso, de la capacidad del centro
+        --y del inventario del  centro
+        
+        SELECT SUM(cantidad_pri_inv) INTO existencia  
+        FROM INVENTARIO_VAC
+        WHERE centro_vac_inv = centro.id_cen;
+        SELECT SUM(cantidad_seg_jv) INTO segundas_dosis
+        FROM JORNADA_VAC
+        WHERE centro_vac_jv = centro.id_cen AND fecha_jv = fecha_actual;
+        SELECT CASE
+                    WHEN existencia < cv.capacidad_cen - segundas_dosis  THEN existencia
+                    ELSE cv.capacidad_cen - segundas_dosis
+                END primeras_dosis  
+        INTO nuevas_primeras_dosis --Guardamos aqui   
+        FROM centro_vac cv 
+        WHERE cv.id_cen = centro.id_cen;
+        
+        
+        --Pregunta si hay insumos, elige lo que haya menos de los insumos (incluido las nuevas primeras dosis)
+        SELECT LEAST(cant_jeringas_sum,cant_alcohol_sum,cant_algodon_sum,cant_par_guantes_sum*10,nuevas_primeras_dosis)
+        INTO nuevas_primeras_dosis
+        FROM SUMINISTROS
+        WHERE centro_vac_sum = 1 AND fecha_sum = TO_DATE('21/07/2021','dd/mm/yyyy');
+        DBMS_OUTPUT.PUT_LINE('nuevas primeras dosis -> ' || nuevas_primeras_dosis);
+        
+        
+        --Guarda el porcentaje de ancianos vacunados
+        SELECT (SELECT  SUM(jv.cantidad_pri_jv)
+                FROM JORNADA_VAC jv 
+                WHERE grupo_etario_jv = 4 AND pais_jv = centro.pais_cv)/pge.cant_hab_pge.cant_total AS porcentaje
+        INTO porcentaje_vacunados
+        FROM PAIS_GE pge
+        WHERE pge.grupo_etario_pge = 4 AND pge.pais_pge =centro.pais_cv;
+
+        --El 40% de los ancianos ya esta vacunado?
+        IF (porcentaje_vacunados > 40) THEN
+
+            --Guarda el porcentaje de adultos vacunados
+            SELECT (SELECT  SUM(jv.cantidad_pri_jv)
+                    FROM JORNADA_VAC jv 
+                    WHERE grupo_etario_jv = 3 AND pais_jv = centro.pais_cv)/pge.cant_hab_pge.cant_total AS porcentaje
+            INTO porcentaje_vacunados
+            FROM PAIS_GE pge
+            WHERE pge.grupo_etario_pge = 3 AND pge.pais_pge = centro.pais_cv;
+
+
+            IF (porcentaje_vacunados > 40) THEN 
+
+                --Guarda el porcentaje de jovenes vacunados
+                SELECT (SELECT  SUM(jv.cantidad_pri_jv)
+                        FROM JORNADA_VAC jv 
+                        WHERE grupo_etario_jv = 2 AND pais_jv = centro.pais_cv)/pge.cant_hab_pge.cant_total AS porcentaje
+                INTO porcentaje_vacunados
+                FROM PAIS_GE pge
+                WHERE pge.grupo_etario_pge = 2 AND pge.pais_pge = centro.pais_cv;
+
+                IF (porcentaje_vacunados > 40) THEN
+                    --Hay mas de 40% de jovenes vacunados. A cada grupo le toca 25%
+                    FOR porc_vac IN porcentajes_vacuna(centro.id_cen)
+                    LOOP
+                        --En cada loop, vacuna a la capacidad menos las segundas dosis que ya 
+                        -- se pusieron, pero multiplicada por la porcion de cada vacuna
+
+                        --25 a cada grupo
+                        UPDATE JORNADA_VAC
+                        SET cantidad_pri_jv = nuevas_primeras_dosis*porc_vac.porcentaje*0.25
+                        WHERE fecha_jv = fecha_actual 
+                            AND vacuna_jv = porc_vac.vacuna_inv
+                            AND pais_jv = centro.pais_cv
+                            AND centro_vac_jv = centro.id_cen;
+
+                        --Resta la vacuna 
+                        UPDATE INVENTARIO_VAC
+                        SET cantidad_pri_inv = cantidad_pri_inv - nuevas_primeras_dosis*porc_vac.porcentaje
+                        WHERE centro_vac_inv = centro.id_cen
+                            AND vacuna_inv = porc_vac.vacuna_inv;
+                                                        
+                    END LOOP;
+                ELSE
+                    
+                    --Hay menos de 40% de adultos vacunados. Toca el 34% a ancianos, 33% a adultos
+                    -- y 33% jovenes
+                    FOR porc_vac IN porcentajes_vacuna(centro.id_cen)
+                    LOOP
+                        --En cada loop, vacuna a la capacidad menos las segundas dosis que ya 
+                        -- se pusieron, pero multiplicada por la porcion de cada vacuna
+
+                        --33% a jovenes y adultos
+                        UPDATE JORNADA_VAC
+                        SET cantidad_pri_jv = nuevas_primeras_dosis*porc_vac.porcentaje*0.33
+                        WHERE fecha_jv = fecha_actual 
+                            AND (grupo_etario_jv = 3 OR grupo_etario_jv = 2)--Jovenes y adultos
+                            AND vacuna_jv = porc_vac.vacuna_inv
+                            AND pais_jv = centro.pais_cv
+                            AND centro_vac_jv = centro.id_cen;
+
+                        --34% para los ancianos
+                        UPDATE JORNADA_VAC
+                        SET cantidad_pri_jv = nuevas_primeras_dosis*porc_vac.porcentaje*0.34
+                        WHERE fecha_jv = fecha_actual 
+                            AND (grupo_etario_jv = 4)--Ancianos
+                            AND vacuna_jv = porc_vac.vacuna_inv
+                            AND pais_jv = centro.pais_cv
+                            AND centro_vac_jv = centro.id_cen;
+
+                        --Resta la vacuna 
+                        UPDATE INVENTARIO_VAC
+                        SET cantidad_pri_inv = cantidad_pri_inv - nuevas_primeras_dosis*porc_vac.porcentaje
+                        WHERE centro_vac_inv = centro.id_cen
+                            AND vacuna_inv = porc_vac.vacuna_inv;
+                    END LOOP;
+
+                END IF;
+
+            ELSE
+
+                --Hay menos de 40% de adultos vacunados. Toca el 50% a ancianos y 50% a adultos
+                FOR porc_vac IN porcentajes_vacuna(centro.id_cen)
+                LOOP
+                    --En cada loop, vacuna a la capacidad menos las segundas dosis que ya 
+                    -- se pusieron, pero multiplicada por la porcion de cada vacuna
+                    UPDATE JORNADA_VAC
+                    SET cantidad_pri_jv = (centro.capacidad_cen - nuevas_segundas_dosis)*porc_vac.porcentaje*0.5
+                    WHERE fecha_jv = fecha_actual 
+                        AND (grupo_etario_jv = 4 OR grupo_etario_jv = 3)
+                        AND vacuna_jv = porc_vac.vacuna_inv
+                        AND pais_jv = centro.pais_cv
+                        AND centro_vac_jv = centro.id_cen;
+
+                    --Resta la vacuna 
+                    UPDATE INVENTARIO_VAC
+                    SET cantidad_pri_inv = cantidad_pri_inv - nuevas_primeras_dosis*porc_vac.porcentaje
+                    WHERE centro_vac_inv = centro.id_cen
+                        AND vacuna_inv = porc_vac.vacuna_inv;
+                    
+                END LOOP;
+
+            END IF;
+
+        ELSE
+
+        --Hay menos de 40% de ancianos vacunados. Toca el 100% a ancianos
+            FOR porc_vac IN porcentajes_vacuna(centro.id_cen)
+            LOOP
+                --En cada loop, vacuna a la capacidad menos las segundas dosis que ya 
+                -- se pusieron, pero multiplicada por la porcion de cada vacuna
+                UPDATE JORNADA_VAC
+                SET cantidad_pri_jv = nuevas_primeras_dosis*porc_vac.porcentaje
+                WHERE fecha_jv = fecha_actual 
+                    AND grupo_etario_jv = 4
+                    AND vacuna_jv = porc_vac.vacuna_inv
+                    AND pais_jv = centro.pais_cv
+                    AND centro_vac_jv = centro.id_cen;
+                
+               --Resta la vacuna 
+                UPDATE INVENTARIO_VAC
+                SET cantidad_pri_inv = cantidad_pri_inv - nuevas_primeras_dosis*porc_vac.porcentaje
+                WHERE centro_vac_inv = centro.id_cen
+                    AND vacuna_inv = porc_vac.vacuna_inv;
+                
+            END LOOP;
+     
+        END IF;
+        --Solo falta restar los insumos
+        UPDATE SUMINISTROS
+        SET cant_jeringas_sum = cant_jeringas_sum - nuevas_primeras_dosis,
+            cant_alcohol_sum = cant_alcohol_sum - nuevas_primeras_dosis,
+            cant_algodon_sum = cant_algodon_sum - nuevas_primeras_dosis,
+            cant_par_guantes_sum = cant_par_guantes_sum - CEIL(nuevas_primeras_dosis/10) --CEIL redondea para arriba
+        WHERE centro_vac_sum = centro.id_cen 
+            AND fecha_sum = fecha_actual;
+
+        
     END LOOP;
 
 
 END;
+
