9583a6967ee14627ebfe8c73cc8954fb0d217fa7
LeoPro420 <jlcontreras.18@est.ucab.edu.ve>
2021-07-06 15:22:11 -0400

    Hice un metodo que muestra un mensaje para resumir lo que paso durante cada semana en la simulacion

--- Modulos/economia.sql
+++ Modulos/economia.sql
@@ -157,7 +157,6 @@ BEGIN
  
     poblacion:=get_poblacion(pais_p,'TOTAL');
     vacunas_a_ordenar:= TRUNC(get_poblacion(pais_p,'TOTAL')/cantidad_de_proveedores); --Divide la población entre la cantidad de proveedores
-    DBMS_OUTPUT.PUT_LINE('Cantidad proveedores: '||cantidad_de_proveedores||'Poblacion: '||poblacion||' Vacunas a ordenar: '||vacunas_a_ordenar);
 
     --Si la cantidad de vacunas a ordenar es menor que la cantidad de vacunas que se gastan en un mes(Lo que tardan en llegar las vacunas) 
     --en ese pais la cantidad de vacunas a ordenar se calcula en base a su capacidad total.
@@ -167,9 +166,10 @@ BEGIN
     WHERE pais_cv = pais_p;
 
     IF(vacunas_a_ordenar < capacidad_total*4) THEN
-    BEGIN
         vacunas_a_ordenar:= capacidad_total*6;
     END;
+
+    DBMS_OUTPUT.PUT_LINE('Cantidad proveedores: '||cantidad_de_proveedores||'Poblacion: '||poblacion||' Vacunas a ordenar: '||vacunas_a_ordenar);
 
     SELECT count(id_vac) INTO cantidad_de_vacunas FROM VACUNA; --Cuenta las vacunas
     
@@ -218,8 +218,8 @@ aprobado_covax BOOLEAN;
 BEGIN
     IF (vacuna_aprobada() = TRUE) THEN
         FOR p IN (SELECT * FROM PAIS)
-         DBMS_OUTPUT.PUT_LINE('El pais: '||p.nombre_pai||' hará lo siguiente:');
         LOOP
+            DBMS_OUTPUT.PUT_LINE('El pais: '||p.nombre_pai||' hará lo siguiente:');
             orden_pen:= orden_pendiente(p.id_pai, fecha_actual);
             IF (orden_pen.id_ord IS NOT NULL) THEN --Chequeamos si hay orden pendiente
                 IF(fecha_actual >= orden_pen.f_entrega_ord) THEN --Chequeamos si ya llegó la orden
@@ -241,7 +241,7 @@ BEGIN
                         SELECT covax_pai INTO covax_p FROM pais WHERE id_pai = p.id_pai;
                         IF(covax_p = 'Y') THEN --pertenece a covax?
                             DBMS_OUTPUT.PUT_LINE('Realizando orden a covax');
-                            aprobado_covax = solitar_orden_covax(p.id_pai,fecha_actual);
+                            aprobado_covax := solcitar_orden_covax(p.id_pai,fecha_actual);
                             IF(aprobado_covax = FALSE) THEN
                                 DBMS_OUTPUT.PUT_LINE('Covax rechazó la orden, realizando orden a otro provedor');
                                 orden_a_proveedor(p.id_pai,fecha_actual);--se pide a alguien mas
