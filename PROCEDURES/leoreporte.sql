b72842100555bc0875af2fa24eafc00904139fc2
LeoPro420 <jlcontreras.18@est.ucab.edu.ve>
2021-08-02 14:29:47 -0400

    Reporte 8 por si acaso

---
+++ PROCEDURES/leoreporte.sql
@@ -0,0 +1,34 @@
+--Reporte 8
+CREATE OR REPLACE PROCEDURE reporte_8(rep_cursor OUT sys_refcursor, pais_p varchar, vacuna_p varchar) IS
+BEGIN
+   OPEN rep_cursor
+   FOR 
+   SELECT p.bandera_pai, r.nombre_pai, r.nombre_vac , r.porcentaje_vacunado, r.procentaje_efsec_detectado
+   FROM(
+      SELECT id_pai, nombre_pai, nombre_vac,TRUNC((SUM(cantidad_seg_jv)/get_poblacion(id_pai,'TOTAL'))*100,2) as porcentaje_vacunado
+      , TRUNC((SUM(cantidad_efsec_jv)/SUM(cantidad_seg_jv))*100,2) as procentaje_efsec_detectado
+      FROM pais
+      JOIN jornada_vac ON pais_jv = id_pai
+      JOIN vacuna ON vacuna_jv = id_vac
+      JOIN jv_efec ON vacuna_jve = id_vac
+      WHERE nombre_pai LIKE nvl(pais_p, nombre_pai)
+      AND nombre_vac LIKE nvl(vacuna_p,nombre_vac)
+      GROUP BY nombre_pai, nombre_vac, id_pai) r
+   JOIN pais p ON p.id_pai = r.id_pai 
+   ORDER BY p.id_pai;
+END;
+
+--Reporte 8 subreporte 1
+CREATE OR REPLACE PROCEDURE reporte_8_subreporte_1(rep_cursor OUT sys_refcursor, pais_p varchar, vacuna_p number, pais_p number) IS
+BEGIN
+   OPEN rep_cursor
+   FOR SELECT id_pai, id_vac, DISCTINCT(nombre_efe)
+      FROM pais
+      JOIN jornada_vac ON pais_jv = id_pai
+      JOIN vacuna ON vacuna_jv = id_vac
+      JOIN jv_efec ON vacuna_jve = id_vac
+      JOIN efecto_secundario ON id_efe = efecto_secundario_jve
+      WHERE id_pai = pais_p
+      AND id_vac = vacuna_p
+      GROUP BY id_pai, id_vac;
+END;
