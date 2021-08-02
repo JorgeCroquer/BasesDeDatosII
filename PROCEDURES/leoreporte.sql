17394f4f99ec8b0c3e3cae667fea8448e5bb4385
LeoPro420 <jlcontreras.18@est.ucab.edu.ve>
2021-08-02 14:36:51 -0400

    case

--- PROCEDURES/leoreporte.sql
+++ PROCEDURES/leoreporte.sql
@@ -6,7 +6,11 @@ BEGIN
    SELECT p.bandera_pai, r.nombre_pai, r.nombre_vac , r.porcentaje_vacunado, r.procentaje_efsec_detectado
    FROM(
       SELECT id_pai, nombre_pai, nombre_vac,TRUNC((SUM(cantidad_seg_jv)/get_poblacion(id_pai,'TOTAL'))*100,2) as porcentaje_vacunado
-      , TRUNC((SUM(cantidad_efsec_jv)/SUM(cantidad_seg_jv))*100,2) as procentaje_efsec_detectado
+      ,
+      CASE 
+         WHEN SUM(cantidad_seg_jv) != 0 THEN TRUNC((SUM(cantidad_efsec_jv)/SUM(cantidad_seg_jv))*100,2) 
+         ELSE 0
+      END as procentaje_efsec_detectado
       FROM pais
       JOIN jornada_vac ON pais_jv = id_pai
       JOIN vacuna ON vacuna_jv = id_vac
